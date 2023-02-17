cfa62b15c1da89e5c55a7b5d12a52fa3b4ed38b3
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-17 03:22:29 -0800

    Puts function and betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/* 
- * practising the structure of C programming
+
+/**
+ * main - entry point
+ * Description:  display the string standard outpu
+ * Return: 0 if true
  */
 int main(void)
-{	/* using puts function*/
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
