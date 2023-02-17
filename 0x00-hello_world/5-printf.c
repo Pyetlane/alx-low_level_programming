ed9f0c788b8e0cf0cbe42367516800a48a61ba58
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-17 03:40:46 -0800

    Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: displaying text using function printf on standard output
+ * Return: 0 if true
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
