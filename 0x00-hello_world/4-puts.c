8b737c14c871a80143fa32288730ded845f69b7c
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-17 02:08:26 -0800

    Puts function in use

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
-
+/* practising the structure of C programming*/
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle"); return (0); }
+{	/* using puts function*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
