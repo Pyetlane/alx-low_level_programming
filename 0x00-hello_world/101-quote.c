3ce5dca9b5fd7018c37acd9a7583e323349ee857
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-17 04:40:54 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Description: prints a text
+ * Return: 1 if true
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
