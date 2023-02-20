17cd4806dce9574746493997e6ae0f2b4f4a5862
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-20 04:14:28 -0800

    combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - entrypoint
+ * Description: different combinations of numbers
+ * Return: 0 if complete
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (j + i != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
