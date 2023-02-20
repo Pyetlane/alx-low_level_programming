5008fadff3bf85e0893dc8b7171930afe3d31952
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-20 04:24:13 -0800

    various combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Results: 0 when done
+ */
+int main(void)
+{
+	int i;
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 when done
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	return (0);
+}
