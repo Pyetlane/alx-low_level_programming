4d7fc380301d3cecd980e8a8c7f238fbeb545181
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-21 04:28:06 -0800

    Alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * print_alphabet function printing alphabet
  * Description: function main uses functionprint_alphabet to print
  * Return: 0 for success
  */
-void print_alphabet(void);// prototype
+void print_alphabet(void);/* prototype*/
 
 void print_alphabet(void)
 {
