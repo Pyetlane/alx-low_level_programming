0f5cebebbb665fe196518ff5c42e6407b16d1a47
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-21 22:33:08 -0800

    Alphabets low case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_alphabet - function printing alphabet
  * Description: function main uses functionprint_alphabet to print
  * Return: 0 for success
  */
-void print_alphabet(void);/* prototype*/
 
 void print_alphabet(void)
 {
@@ -14,8 +12,8 @@ void print_alphabet(void)
 	a = 'a';
 	while (a <= 'z')
 	{
-		putchar(a);
+		_putchar(a);
 		a++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
