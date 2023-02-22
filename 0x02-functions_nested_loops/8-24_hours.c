19087c119194b54b3301808101b6ab0ab07cc220
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-22 13:28:38 -0800

    ( times table

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * jack_bauer - prints 24 hours time
+ *
+ * Return: 24 hours time
+ */
+void jack_bauer(void)
+{
+	
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - prints time table
+ *
+ * Return: multiples of nine
+ */
+void times_table(void)
+{
+	int i, j, multiple;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			multiple = i * j;
+			_putchar(multiple + '0');
+			_putchar(',');
+			_putchar(' ');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
