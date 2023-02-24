536624f3c7c185afe34ded3b2262f3e97f6db042
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-24 13:28:01 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,18 @@ void more_numbers(void)
 	int i, j;
 	
 	i = 0;
-	while (i < 15)
+	while (i < 10)
 	{
 		j = 0;
 		while (j < 15)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+			{
+				_putchar('1');
+				_putchar(j % 10 + '0');
+			}
+			else
+				_putchar(j + '0');
 			j++;
 		}
 		i++;
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - diagonal printing
+ * @n: Parameter of diagonal function
+ *
+ * Return: diagonal printing
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n + 1)
+	{
+		if (n > 0)
+		{
+			_putchar()
+		}
+	}
+}
