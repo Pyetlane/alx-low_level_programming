0c6083e1ca231003817657a95ec6d58a173048e7
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-24 11:39:00 -0800

    Multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiple two integers
+ * @a: firsdt parameter
+ * @b: second parameter
+ *
+ * Return: multiple of two integere
+ */
+int mul(int a, int b)
+{
+	int multiple;
+
+	multiple = a * b;
+	return (multiple);
+}
