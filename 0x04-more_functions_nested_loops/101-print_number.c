50fa27ef6864e19d0d96415f3b0c1d055a78cc41
Pyetlane <tlhologelolekoane@gmail.com>
2023-02-26 05:39:32 -0800

    Print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_number - Prints numbers
+ * @n: parameter of print number function
+ *
+ * Return: numbers
+ */
+void print_number(int n)
+{
+	_putchar(n % 1500 + '0');
+}
