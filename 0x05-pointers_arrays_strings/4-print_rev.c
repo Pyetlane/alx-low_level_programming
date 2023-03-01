4effd4ae145e8477fdb92a4f20c5101cc5d849d8
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-01 02:45:26 -0800

    reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - reverse string
+ * @s: char parameter
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+	
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
