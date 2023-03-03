6944eab3e8c1e1352f33ac5826948f51ab350bd9
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-02 17:59:07 -0800

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the content
+ * @a: 1st integer
+ * @n: second integer
+ *
+ * Return: reverssed array
+ */
+void reverse_array(int *a, int n)
+{
+	
+}
