d373fa919c17eb41669ca188415654dcce025025
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-02 18:48:12 -0800

    capitalize

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - capitalizes string
+ * @a: takes a character
+ *
+ * Return: Capitalized string
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
