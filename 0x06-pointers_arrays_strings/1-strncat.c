9cb497db37f3f7a78a0bd8d5e284ddf24da563ec
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-02 16:40:28 -0800

    another strings concaternates

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strncat - concatenate strings
+ * @n: integer parameter
+ * @dest: 1st  string
+ * @src: second string
+ *
+ * Return: one string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
