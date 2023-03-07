962dddc10b3446481b3c9394c4a6f31e79feb8ae
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-07 04:58:00 -0800

    locates string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: first input
+ * @needle: second parameter
+ *
+ * Return: pointer of location
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return (0);
+}
