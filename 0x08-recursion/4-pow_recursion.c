f46d9e3d43e7bb90afa2547bfaf37ab23b2db5f8
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-08 11:54:07 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,8 +12,16 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
 	else
 	{
-		return (_pow_recursion(x + x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
