abed3d83cdbb92478f4be918ff9fba6aa5f0ad32
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-09 11:02:49 -0800

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int calc(int n, int count);
 /**
  * is_prime_number - checks if prime number
  * @n: input
@@ -7,12 +8,30 @@
  */
 int is_prime_number(int n)
 {
-	if (n % is_prime_number(n - 1) == 0 )
+	if (n <= 1)
 	{
 		return (0);
 	}
 	else
 	{
+		return (calc(n, n - 1));
+	}
+}
+/**
+ * calc - determines the prime
+ * @n: given input
+ * @count: second parameter to count
+ * Return: 1 or 0
+ */
+int calc(int n, int count)
+{
+	if  (count == 1)
+	{
 		return (1);
 	}
+	else if (n % count == 0 && count > 0)
+	{
+		return (0);
+	}
+	return (calc(n, count - 1));
 }
