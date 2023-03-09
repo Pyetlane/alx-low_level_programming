e672cbc5786434421e62792eb4720ca20252c69e
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-09 10:40:53 -0800

    squares

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int natural(int n, int guess);
 /**
  * _sqrt_recursion - computes a square root
  * @n: input
@@ -7,5 +8,31 @@
  */
 int _sqrt_recursion(int n)
 {
-	if ()
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (natural(n, 0));
+	}
+}
+
+/**
+ *  natural  - guess the natural numberr
+ * @n: 1st input
+ * @guess: 2nd parameter than guesses the number
+ * Return: natural number if passes otherwise -1
+ */
+int natural(int n, int guess)
+{
+	if (guess * guess > n)
+	{
+		return (-1);
+	}
+	else if (guess * guess == n)
+	{
+		return (guess);
+	}
+	return (natural(n, guess + 1));
 }
