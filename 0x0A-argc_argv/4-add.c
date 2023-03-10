f3bb95908372c2f4c94818594b268e9dc625f2e1
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-10 11:27:32 -0800

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - entry point
  * @argc: counter
@@ -8,5 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
-	
+	int i, add;
+
+	i = 1;
+	add = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		while (i < argc)
+		{
+			add = add + strtol(argv[i], NULL, 10);
+			i++;
+		}
+		printf("%d\n", add);
+	}
+
+	return (0);
 }
