5ecc2942c701006981ec5779959a0ecbb63a8abf
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-10 12:17:40 -0800

    chnage

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: couinter
+ * @argv: array of strings
+ *
+ * Return: prints the minim number
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+
+	if (i != argc)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+	
+	}
+}
