cc5d0315ec90f9fcea990dc417469f9c3c80c74f
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-10 03:54:23 -0800

    name of file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: counter
+ * @argv: array of strings
+ *
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
