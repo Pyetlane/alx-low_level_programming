d769ef93bc40306169a59f414cf329756d38d1c9
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-13 05:59:03 -0700

    cretae

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * create_array - creates an arry of chars
+ * @size: unsigned parameter
+ * #c: second parametr
+ *
+ * Return: NULL if 0 chars or pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	
+}
