2aac61aa618b66dccfc99848a6b7cbf04f7a4331
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-14 05:56:31 -0700

    2-d array pointer

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - returns pointer to a 2-d arry
+ * @width: 1stparametr
+ * @height:second parameter
+ *
+ * Return: a pointer to 2-D array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int *array = [];
+
+	if ((width <= 0) || (height <= 0))
+	{
+		return (NULL);
+	}
+	else
+	{
+	
+	}
+}
