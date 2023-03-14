5eaf6cdc110e73f566360623e6bac102b92aa6ee
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-14 06:01:36 -0700

    freeing a grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees a grid
+ * @grid: 2-D array input
+ * @height: wnd input
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	
+}
