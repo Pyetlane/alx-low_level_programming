71530d4edba077ce5aacbc7bf7961804935ae491
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-15 02:25:32 -0700

    101

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strtow - splits astring into words
+ * @str: string input
+ *
+ * Return: pointer of string of words
+ */
+char **strtow(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	return (str);
+}
