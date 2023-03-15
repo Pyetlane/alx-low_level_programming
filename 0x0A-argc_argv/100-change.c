22966e6113e7ba7123080bf42f0dab874b49ae2c
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-15 02:51:44 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,14 +9,35 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 1;
+	int coins[] = {25, 10, 5, 2, 1};
+	int ipar, i, diff;
 
-	if (i != argc)
+	diff = 0;
+	if (argc != 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
 	else
 	{
-	
+		ipar = atoi(argv[1]);
+		if (ipar < 0)
+		{
+			printf("0\n");
+			return (0);
+		}
+
+		i = 0;
+		while (i < 5 && ipar >= 0)
+		{
+			while (ipar >= coins[i])
+			{
+				diff++;
+				ipar -= coins[i];
+			}
+			i++;
+		}
+		printf("%d\n", diff);
+		return (0);
 	}
 }
