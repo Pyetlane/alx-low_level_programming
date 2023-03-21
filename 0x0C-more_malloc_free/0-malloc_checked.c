123ec9bb1fcee5dc74c83b533948876ebbd22c6f
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-21 06:32:48 -0700

    malloc-checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - malloc to locate memory
+ * @b: integer parameter
+ *
+ * Return: return 98 when fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		return (98);
+	}
+	return (p);
+}
