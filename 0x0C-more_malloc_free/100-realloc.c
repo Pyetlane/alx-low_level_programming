ea270ef6c5f4d001c6c703723b717de9a162cb24
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-21 22:49:52 -0700

    reallocates a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,30 +18,32 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-
 	if (new_size == 0 && ptr)
-	{
 		return (NULL);
-	}
-
 	if (!ptr)
-	{
 		return (malloc(new_size));
-	}
-
 	p1 = malloc(new_size);
 	if (!p1)
-	{
 		return (NULL);
-	}
-
 	p2 = ptr;
 	if (new_size < old_size)
 	{
 		i = 0;
 		while (i < new_size)
 		{
+			p1[i] = p2[i];
 			i++;
 		}
 	}
+	if (new_size > old_size)
+	{
+		i = 0;
+		while (i < old_size)
+		{
+			p1[i] = p2[i];
+			i++;
+		}
+	}
+	free(ptr);
+	return (p1);
 }
