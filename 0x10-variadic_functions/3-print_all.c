36eb516c27825ad8e331004b06b0861bbca5db2a
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-24 15:48:38 -0700

    Prints all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - prints anything
+ * @format: parameter
+ *
+ * Return: nothing if success
+ */
+void print_all(const char * const format, ...)
+{
+
+}
