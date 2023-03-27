c0e81032e0492a66ebfa773dd65c91db6383859a
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-27 14:41:15 -0700

    dog structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Tasks for data structures and tyoedef practise
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+/**
+ * struct dog - structure
+ * @name: 1st struct member
+ * @age: 2nd struct member
+ * @owner: 3rd struct member
+ *
+ * Description: dog data structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
