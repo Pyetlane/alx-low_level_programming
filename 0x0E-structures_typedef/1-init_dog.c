d62a062133991398cea89673f9ef50be0150e079
Pyetlane <tlhologelolekoane@gmail.com>
2023-03-27 14:58:13 -0700

    initialize dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#ifndef INIT_DOG_H
+#define INIT_DOG_H
+#include "dog.h"
+/**
+ * init_dog - initialize dog
+ * @d: pointer to truct dog
+ * @name: 1st parameter
+ * @age: 2nd parameter
+ * @owner: 3rd parameter
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+#endif
